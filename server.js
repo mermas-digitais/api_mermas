5f0ac246e94d3fb180145a474540ac4f7108d4ca
julysNICK <julystube@gmail.com>
2022-12-05 15:46:11 -0300

    connected mondb

--- server.js
+++ server.js
@@ -1,16 +1,25 @@
 var express = require('express')
+const mongoose = require('mongoose')
 app = express()
+const dotenv = require('dotenv')
+dotenv.config()
 
-var routers = require('./routers/index')
+const routes = require('./routers/PublicationRouter.js')
 
 
-routers(app)
+mongoose.connect(
+  process.env.MONGODBURL,
+).then(() => {
+  console.log('Connected to database')
+}).catch(() => {
+  console.log('Connection failed')
+})
+
+app.use('/api', routes);
 
 app.listen(3333, function () {
   console.log('Server is running on port 3333')
 })
 
-var router = express.Router();
 
 
-app.use('/api', router);
