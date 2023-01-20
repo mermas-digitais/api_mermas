5387559d2c96ffa8af72a48087b21840ce99b980
julysNICK <julystube@gmail.com>
2023-01-20 17:09:07 -0300

    fix .env

--- config/cloudinary.js
+++ config/cloudinary.js
@@ -1,13 +1,9 @@
 const cloudinary = require('cloudinary').v2;
 
 cloudinary.config({
-  cloud_name: 'dywwplkdp',
-  api_key:
-    '782172994675616',
-  api_secret: 'pLQ6nPQOo9ZAEU5frEhprZXL2RY',
-
-
-
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 module.exports = cloudinary;
